69a1d35a240d41697d6b012d43144017f463d3f5
mehedi hasan <mhshohan04@gmail.com>
2025-05-27 16:00:21 +0600

    images are successfullly renders in UI

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-const searchForm = document.getElementById('search-form');
-const searchBox = document.getElementById('search-box');
-const searchResult= document.getElementById('search-result');
-const showMoreBtn = document.getElementById('show-more-btn');
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult= document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
 
-const accessKey = "5tmeNKdIQJ0Srn6TPsARx3Kqia_haPCGJyNA2X0lLKc";
 let keyword = '';
 let page = 1;
 
 async function searchImages() {
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&
-  client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=TjO5wJtCFTLLaV-R50GxnhSp864-tOBB_uN4cMR5O5U`;
 
   const response = await fetch(url);
   const data = await response.json();
 
-  console.log(data);
+  // console.log(data);
+  const results = data.results;
+
+  results.map((result)=>{
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  })
 }
+
+searchForm.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  page = 1;
+  searchImages()
+})
