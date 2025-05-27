fc29b7bdfa829d0e966476be0d6913d071309a99
mehedi hasan <mhshohan04@gmail.com>
2025-05-27 11:51:13 +0600

    call api

---
+++ script.js
@@ -0,0 +1,19 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult= document.getElementById('search-result');
+const showMoreBtn = document.getElementById('show-more-btn');
+
+const accessKey = "5tmeNKdIQJ0Srn6TPsARx3Kqia_haPCGJyNA2X0lLKc";
+let keyword = '';
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&
+  client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+}
